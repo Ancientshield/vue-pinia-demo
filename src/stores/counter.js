1ecd1d7ec2de23fd84903009eb6baf87ad5ab0c1
Ancientshield <ancientsword520@gmail.com>
2023-02-02 13:32:42 +0800

    finished separate API to pinia

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,24 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, reactive, computed } from 'vue';
+import { defineStore } from 'pinia';
+import axios from 'axios';
 
 export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+	const count = ref(0);
+	const doubleCount = computed(() => count.value * 2);
+	function increment() {
+		count.value++;
+	}
 
-  return { count, doubleCount, increment }
-})
+	const dataList = reactive({});
+
+	const dataAPI = async () => {
+		try {
+			const res = await axios.get('https://bible.fhl.net/json/qb.php');
+			dataList.value = res.data;
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return { count, doubleCount, increment, dataAPI, dataList };
+});
